1d2ff785631e60b9856e12ceb591dddda88a07ca
Juan David Sandoval Salvador <sandovaldavid2201@gmail.com>
2025-11-30 01:21:43 -0500

    🔥 remove unused endpoint

--- src/app/api/hello/route.js
+++
@@ -1,9 +0,0 @@
-/*
-import {NextResponse} from "next/server";
-import {pool} from "@/libs/mysql";
-
-export async function GET() {
-  const result = await pool.query("SELECT NOW() as Now");
-  console.log(result[0].Now);
-  return NextResponse.json({message: `Time: ${result[0].Now}`})
-}*/
